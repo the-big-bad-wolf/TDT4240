bd94644c1fd38eb7224078b0a88b4afb604eb641
Sophie Kilders <ge96hax@mytum.de>
2023-03-03 17:09:18 +0100

    Initial commit

---
+++ desktop/src/com/mygdx/shapewars/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.shapewars;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.shapewars.ShapeWars;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ShapeWars");
+		new Lwjgl3Application(new ShapeWars(), config);
+	}
+}
