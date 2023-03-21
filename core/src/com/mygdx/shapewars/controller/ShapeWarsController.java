849220586001846ee9aaebc20f91e868208c85f6
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-03-21 14:22:12 +0100

    test new socket config

--- core/src/com/mygdx/shapewars/controller/ShapeWarsController.java
+++ core/src/com/mygdx/shapewars/controller/ShapeWarsController.java
@@ -17,8 +17,12 @@ import com.mygdx.shapewars.view.MainMenuView;
 import com.mygdx.shapewars.view.ShapeWarsView;
 import com.badlogic.gdx.Net.Protocol;
 
+
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ShapeWarsController {
 
@@ -40,27 +44,46 @@ public class ShapeWarsController {
           model.update();
         } else {
             if (Gdx.input.isKeyPressed(Input.Keys.F)) {
-                ServerSocketHints hints = new ServerSocketHints();
-                hints.acceptTimeout=0;
-                ServerSocket server = Gdx.net.newServerSocket(Protocol.TCP,5000,hints);
-                Socket acceptedsocket = server.accept(null);
-                try {
-                    InputStream stream = acceptedsocket.getInputStream();
-                    System.out.println(stream.read());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                ServerSocketHints serverSocketHint = new ServerSocketHints();
+                // 0 means no timeout.  Probably not the greatest idea in production!
+                serverSocketHint.acceptTimeout = 0;
 
-                currentScreen = shapeWarsView;
-                currentScreen.show();
+                // Create the socket server using TCP protocol and listening on 9021
+                // Only one app can listen to a port at a time, keep in mind many ports are reserved
+                // especially in the lower numbers ( like 21, 80, etc )
+                ServerSocket serverSocket = Gdx.net.newServerSocket(Protocol.TCP, 9021, serverSocketHint);
+
+                // Loop forever
+                while(true){
+                    // Create a socket
+                    System.out.println("testbefore");
+                    Socket socket = serverSocket.accept(null);
+
+                    // Read data from the socket into a BufferedReader
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    try {
+                        // Read to the next newline (\n) and display that text on labelMessage
+                        System.out.println(buffer.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //currentScreen = shapeWarsView;
+                //currentScreen.show();
             }
             if (Gdx.input.isKeyPressed(Input.Keys.U)) {
-                SocketHints hints = new SocketHints();
-                Socket socket = Gdx.net.newClientSocket(Protocol.TCP, "10.0.0.17", 5000, hints);
-                try{
-                    socket.getOutputStream().write(99);}
-                catch (IOException e) {
-                    throw new RuntimeException(e);
+                SocketHints socketHints = new SocketHints();
+                // Socket will time our in 4 seconds
+                socketHints.connectTimeout = 4000;
+                //create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port 9021
+                System.out.println("test");
+                Socket socket = Gdx.net.newClientSocket(Protocol.TCP, "10.0.0.17", 9021, socketHints);
+                try {
+                    // write our entered message to the stream
+                    socket.getOutputStream().write(99);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
 
             }
