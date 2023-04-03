fe6afb5de17fdcd80e72d1ccbd0eb43fc0d3cab1
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-04-04 00:59:36 +0200

    fixed android launch

--- android/src/com/mygdx/shapewars/AndroidLauncher.java
+++ android/src/com/mygdx/shapewars/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.shapewars.ShapeWars;
+import com.mygdx.shapewars.controller.ShapeWarsController;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new ShapeWars(), config);
+		initialize(new ShapeWarsController(), config);
 	}
 }
