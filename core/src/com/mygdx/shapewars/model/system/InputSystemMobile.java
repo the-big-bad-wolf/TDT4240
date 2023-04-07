ca3afb79e1add7fbd181963b5fb5fe425f38e154
leonhardbenkert <ge96cej@mytum.de>
2023-04-07 15:20:46 +0200

    Fix: Revert joystick behaviour

--- core/src/com/mygdx/shapewars/model/system/InputSystemMobile.java
+++ core/src/com/mygdx/shapewars/model/system/InputSystemMobile.java
@@ -1,7 +1,6 @@
 package com.mygdx.shapewars.model.system;
 
 import static com.mygdx.shapewars.config.GameConfig.MAX_SPEED;
-import static com.mygdx.shapewars.config.GameConfig.MAX_TURN_RATE;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.MathUtils;
@@ -35,37 +34,36 @@ public class InputSystemMobile extends InputSystem {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         movingJoystick = joystick.getOuterCircle().contains(screenX, Gdx.graphics.getHeight() - screenY);
-        return false;
+        return false; // standard return value
     }
 
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         joystick.getInnerCircle().setPosition(joystick.getOuterCircle().x, joystick.getOuterCircle().y);
         inputValue = 0;
-        inputDirection = 0;
         movingJoystick = false;
-        return false;
+        return false; // standard return value
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        if (!movingJoystick)
-            return false;
+        if (movingJoystick) {
+            float deltaX = screenX - 275 - joystick.getOuterCircle().x;
+            float deltaY = Gdx.graphics.getHeight() - screenY - joystick.getOuterCircle().y;
+            float deltaLength = (float) Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 
-        float deltaX = screenX - 275 - joystick.getOuterCircle().x; // todo sophie why did i need the 275 here? something with the map borders?
-        float deltaY = Gdx.graphics.getHeight() - screenY - joystick.getOuterCircle().y;
+            deltaX = MathUtils.clamp(deltaX, -outerCircleRadius, outerCircleRadius);
+            deltaY = MathUtils.clamp(deltaY, -outerCircleRadius, outerCircleRadius);
+            float angle = MathUtils.atan2(deltaY, deltaX) * MathUtils.radiansToDegrees;
 
-        deltaX = MathUtils.clamp(deltaX, -outerCircleRadius, outerCircleRadius);
-        deltaY = MathUtils.clamp(deltaY, -outerCircleRadius, outerCircleRadius);
-
-        joystick.getInnerCircle().setPosition(joystick.getOuterCircle().x + deltaX, joystick.getOuterCircle().y + deltaY);
-
-        inputDirection = -deltaX / outerCircleRadius * MAX_TURN_RATE;
-        inputValue = deltaY / outerCircleRadius * MAX_SPEED;
-        return false;
+            joystick.getInnerCircle().setPosition(joystick.getOuterCircle().x + deltaX, joystick.getOuterCircle().y + deltaY);
+            inputDirection = angle;
+            inputValue = MathUtils.clamp(deltaLength, 0, outerCircleRadius) / outerCircleRadius * MAX_SPEED;
+        }
+        return false; // standard return value
     }
 
     /*
-     * todo: decide on joystick implementation, add firing button
+     * todo: add firing button
      */
 }
