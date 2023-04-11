d70999d61ff3c7ebe56d9f5b77098724eedda2cf
The Big Bad Wolf <78231350+the-big-bad-wolf@users.noreply.github.com>
2023-04-11 13:39:55 +0200

    moved gamemodel creation from constructor to create()

--- core/src/com/mygdx/shapewars/controller/ShapeWarsController.java
+++ core/src/com/mygdx/shapewars/controller/ShapeWarsController.java
@@ -1,7 +1,6 @@
 package com.mygdx.shapewars.controller;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.mygdx.shapewars.config.Launcher;
 import com.mygdx.shapewars.config.Role;
@@ -13,40 +12,31 @@ import com.mygdx.shapewars.view.ShapeWarsView;
 public class ShapeWarsController extends Game {
 
     public ShapeWarsModel shapeWarsModel;
-    public final GameModel gameModel;
+    public GameModel gameModel;
     public Screen currentScreen;
     public Launcher launcher;
 
     public ShapeWarsController(Launcher launcher) {
-        this.gameModel = new GameModel(launcher);
         this.launcher = launcher;
+    }
+
+    public void create() {
+        this.gameModel = new GameModel(launcher);
+        this.setScreen(new MainMenuView(this));
+    }
+
+    public void render() {
+        if (this.screen instanceof ShapeWarsView) {
+            shapeWarsModel.update();
+        }
+        super.render();
     }
 
     public void generateShapeWarsModel(Role role, String serverIpAddress) {
         this.shapeWarsModel = new ShapeWarsModel(this, this.gameModel, role, serverIpAddress);
     }
 
-    public void update() {
-        if (currentScreen instanceof ShapeWarsView) {
-            shapeWarsModel.update();
-        }
-        currentScreen.render(Gdx.graphics.getDeltaTime());
-    }
-
     public void dispose() {
         gameModel.batch.dispose();
-    }
-
-    @Override
-    public void create() {
-        this.setScreen(new MainMenuView(this));
-    }
-
-    @Override
-    public void render() {
-        if (this.screen instanceof ShapeWarsView) {
-            shapeWarsModel.update();
-        }
-        super.render();
     }
 }
