8614044015bf69f6d6130757144cf866de8b676f
sindreeh <sindreeh@stud.ntnu.no>
2023-04-13 17:15:57 +0200

    Downloaded all ship sprites and changed tanks to ships.

--- core/src/com/mygdx/shapewars/config/GameConfig.java
+++ core/src/com/mygdx/shapewars/config/GameConfig.java
@@ -10,9 +10,9 @@ import com.mygdx.shapewars.model.components.VelocityComponent;
 public abstract class GameConfig {
     public static final int MAX_SPEED = 5;
     public static final int MAX_TURN_RATE = 3;
-    public static final int TANK_WIDTH = 75;
+    public static final int TANK_WIDTH = 100;
     public static final int TANK_HEIGHT = 75;
-    public static final String FRIENDLY_TANK_SKIN = "tank_graphics.png";
-    public static final String ENEMY_TANK_SKIN = "tank_graphics.png";
+    public static final String FRIENDLY_TANK_SKIN = "player_full_health.png";
+    public static final String ENEMY_TANK_SKIN = "enemy_full_health.png";
     public static final Family TANK_FAMILY = Family.all(PositionComponent .class, VelocityComponent .class, SpriteComponent .class, HealthComponent .class, IdentityComponent.class).get();
 }
