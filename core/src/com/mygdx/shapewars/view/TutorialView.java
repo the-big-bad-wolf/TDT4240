07b8400aef45e40367d1c2f5eed62a55f23dedf1
Sophie Kilders <ge96hax@mytum.de>
2023-04-18 19:57:50 +0200

    Fix of viewport bug

--- core/src/com/mygdx/shapewars/view/TutorialView.java
+++ core/src/com/mygdx/shapewars/view/TutorialView.java
@@ -5,6 +5,7 @@ import java.net.UnknownHostException;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,6 +13,8 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.shapewars.controller.ShapeWarsController;
 
 public class TutorialView implements Screen {
@@ -23,50 +26,20 @@ public class TutorialView implements Screen {
 	private Sprite imageSprite;
 	private Sprite backgroundSprite;
 	private SpriteBatch batch;
+	private ExtendViewport extendViewport;
 
 	public TutorialView(ShapeWarsController controller, int sourceInt) {
 		this.controller = controller;
 		this.stage = new Stage();
 		this.uiBuilder = new UIBuilder(this.stage);
 		this.sourceInt = sourceInt;
+		OrthographicCamera camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.update();
 		batch = new SpriteBatch();
+		extendViewport = new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		stage.setViewport(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera));
 		buildUI();
-
-	}
-
-	private void buildUI() {
-		float nextButtonsWidth = 250f;
-		float nextButtonsHeight = 80f;
-		float nextButtonXPos = Gdx.graphics.getWidth() / 2f - 250 / 2;
-		float nextButtonYPos = 0;
-
-		nextButton = uiBuilder.buildButton("Next", nextButtonsWidth, nextButtonsHeight,
-				nextButtonXPos,
-				nextButtonYPos,
-				"redVersion");
-
-		addActionsToUI();
-	}
-
-	private void addActionsToUI() {
-		nextButton.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-
-				try {
-					if (sourceInt == 0) {
-						controller.setScreen(new HostView(controller));
-					}
-					if (sourceInt == 1) {
-						controller.setScreen(new ClientView(controller));
-					}
-
-				} catch (NullPointerException | UnknownHostException nullPointerException) {
-					System.out.println("No controller found");
-				}
-
-			}
-		});
 	}
 
 	@Override
@@ -76,8 +49,9 @@ public class TutorialView implements Screen {
 
 		Texture image = new Texture(Gdx.files.internal("mainMenu/tutorial.png"));
 		imageSprite = new Sprite(image);
-		imageSprite.setScale(1.1f);
-		imageSprite.setCenter(Gdx.graphics.getWidth()/2f,Gdx.graphics.getHeight()/2f);
+		imageSprite.setScale(1f);
+		imageSprite.setSize(Gdx.graphics.getWidth()-200, Gdx.graphics.getHeight()-100);
+		imageSprite.setPosition(7, (Gdx.graphics.getHeight()- imageSprite.getHeight())/2f);
 		//imageSprite.setPosition(Gdx.graphics.getWidth()/2f, Gdx.graphics.getHeight()/2f);
 
 		Gdx.input.setInputProcessor(stage);
@@ -95,6 +69,8 @@ public class TutorialView implements Screen {
 		controller.gameModel.batch.end();
 
 		batch.begin();
+		backgroundSprite.setSize(stage.getViewport().getWorldWidth(), stage.getViewport().getWorldHeight());
+		backgroundSprite.setPosition((stage.getViewport().getWorldWidth() - backgroundSprite.getWidth())/2, (stage.getViewport().getWorldHeight()- backgroundSprite.getHeight()) / 2);
 		backgroundSprite.draw(batch);
 		imageSprite.draw(batch);
 		batch.end();
@@ -129,5 +105,40 @@ public class TutorialView implements Screen {
 	@Override
 	public void dispose() {
 		stage.dispose();
+	}
+
+	private void buildUI() {
+		float nextButtonsWidth = 180f;
+		float nextButtonsHeight = 100f;
+		float nextButtonXPos = Gdx.graphics.getWidth() - nextButtonsWidth;
+		float nextButtonYPos = (Gdx.graphics.getHeight() - nextButtonsHeight)/2f;
+
+		nextButton = uiBuilder.buildButton("Next", nextButtonsWidth, nextButtonsHeight,
+				nextButtonXPos,
+				nextButtonYPos,
+				"redVersion");
+
+		addActionsToUI();
+	}
+
+	private void addActionsToUI() {
+		nextButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+
+				try {
+					if (sourceInt == 0) {
+						controller.setScreen(new HostView(controller));
+					}
+					if (sourceInt == 1) {
+						controller.setScreen(new ClientView(controller));
+					}
+
+				} catch (NullPointerException | UnknownHostException nullPointerException) {
+					System.out.println("No controller found");
+				}
+
+			}
+		});
 	}
 }
